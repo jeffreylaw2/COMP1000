862fab5a3942bd051b265154c96789828c5d0e76
Jeffrey Law <lawj@wit.edu>
2016-01-28 21:04:13 -0500

    Lab 2 Done For Reals, please disregard my last submission

--- workspace/Submissions/src/Unit.java
+++ workspace/Submissions/src/Unit.java
@@ -5,81 +5,85 @@ public class Unit
 
 	public static double lengthConversion(double measurement, String fromUnit, String toUnit) {
 		// Convert everything to some base unit (everything to, say, mm)
-		if (fromUnit.equals("m")) {
-		} else if (fromUnit.equals("km") && toUnit.equals("m")) {
-			System.out.print(measurement/1000);		
-			return measurement;
-		} else if (fromUnit.equals("km") && toUnit.equals("mi")) {
-			System.out.print(measurement/0.621);		
-			return measurement;
+		if (fromUnit.equals("m") && (toUnit.equals("km"))) {
+			System.out.print(measurement*1000);
+		} else if (fromUnit.equals("m") && toUnit.equals("mi")) {
+			System.out.print(measurement*0.000621371);
 			
 		} else if (fromUnit.equals("mi") && toUnit.equals("m")) {
-			System.out.print(measurement/0.000621371);
-			return measurement;
+			System.out.print(measurement*1609.34);
 		} else if (fromUnit.equals("mi") && toUnit.equals("km")) {
 			System.out.print(measurement*1.60934);
-			return measurement;
-		// Convert everything from the base unit to the desired unit
-		} if (toUnit.equals("km")) {
-			System.out.print(measurement*1000);
-			return measurement;
-		} else if (toUnit.equals("mi")){
-			System.out.print(measurement*0.000621371);
-			return measurement;
-		}
-		return measurement;
+	
+		} else if (fromUnit.equals("km") && toUnit.equals("m")) {
+			System.out.print(measurement/1000);
+		} else if (fromUnit.equals("km") && toUnit.equals("mi")) {
+			System.out.print(measurement*0.62137);
+		} return measurement;
 	}
 	
 
 	public static double temperatureConversion(double measurement, String fromUnit, String toUnit) {
 		// Convert everything to some base unit (everything to, say, mm)
-		if (fromUnit.equals("F")) {
+		if (fromUnit.equals("F") && (toUnit.equals("C"))) {
+			System.out.print((measurement-32)*5/9);
+		} else if (fromUnit.equals("F") && toUnit.equals("K")) {
+			System.out.print((measurement-32)*5/9+273);
 		} else if (fromUnit.equals("C") && toUnit.equals("F")) {
 			System.out.print(measurement*9/5+32);
-			return measurement;
 		} else if (fromUnit.equals("C") && toUnit.equals("K")) {
 			System.out.print(measurement+273);
-			return measurement;
-		
-		// Convert everything from the base unit to the desired unit
-		} if (toUnit.equals("C")) {
-			System.out.print((measurement-32)*5/9);
-		} else if (toUnit.equals("K")) {
-			System.out.print(((measurement-32)*5/9)+273);
-		}
-		return measurement;
+		} else if (fromUnit.equals("K") && toUnit.equals("C")) {
+			System.out.print(measurement-273);
+		} else if (fromUnit.equals("K") && toUnit.equals("F")) {
+			System.out.print((measurement-273)*9/5+32);
+		} return measurement;
 	}
 	public static double timeConversion(double measurement, String fromUnit, String toUnit) {
 		// Convert everything to some base unit (everything to, say, mm)
-		if (fromUnit.equals("sec")) {
-		} else if (fromUnit.equals("min")) {
-			if (toUnit.equals("sec"));
+		if (fromUnit.equals("sec") && (toUnit.equals("min"))) {
 			System.out.print(measurement/60);
-			
-		}
-		
-		// Convert everything from the base unit to the desired unit
-		if (toUnit.equals("min")) {
+		} else if (fromUnit.equals("sec") && toUnit.equals("hr")) {
+			System.out.print(measurement/3600);
+		} else if (fromUnit.equals("min") && toUnit.equals("sec")) {
 			System.out.print(measurement*60);
-		} else if (toUnit.equals("hr"));
-		    System.out.print(measurement*3600);
+		} else if (fromUnit.equals("hr") && toUnit.equals("min")) {
+			System.out.print(measurement*60);
+		} else if (fromUnit.equals("min") && toUnit.equals("hr")) {
+			System.out.print(measurement/60);
+		} else if (fromUnit.equals("hr") && toUnit.equals("sec")) {
+			System.out.print(measurement*3600);
+		}
+	
 		return measurement;
 	}
 	public static double massConversion(double measurement, String fromUnit, String toUnit) {
 		// Convert everything to some base unit (everything to, say, mm)
-		if (fromUnit.equals("g")) {
-		} else if (fromUnit.equals("kg")) {
-			if (toUnit.equals("g"));
+			if (fromUnit.equals("g") && (toUnit.equals("kg"))) {
+				System.out.print(measurement*1000);
+			} else if (fromUnit.equals("kg") && toUnit.equals("g")) {
 				System.out.print(measurement/1000);
-				
-		}
-		
-		// Convert everything from the base unit to the desired unit
-		if (toUnit.equals("kg")) {
-			System.out.print(measurement*1000);
-		} else if (toUnit.equals("lbs"));
-		    System.out.print(measurement*0.00220462);
-		return measurement;
+			} else if (fromUnit.equals("oz") && toUnit.equals("lbs")) {
+				System.out.print(measurement/16);
+			} else if (fromUnit.equals("lbs") && toUnit.equals("oz")) {
+				System.out.print(measurement*16);
+			} else if (fromUnit.equals("g") && toUnit.equals("lbs")) {
+				System.out.print(measurement*0.00220462);
+			} else if (fromUnit.equals("lbs") && toUnit.equals("g")) {
+				System.out.print(measurement*453.592);
+			} else if (fromUnit.equals("lbs") && toUnit.equals("kg")) {
+				System.out.print(measurement/2.2);
+			} else if (fromUnit.equals("kg") && toUnit.equals("lbs")) {
+				System.out.print(measurement*2.2);
+			} else if (fromUnit.equals("g") && toUnit.equals("oz")) {
+				System.out.print(measurement*0.035274);
+			} else if (fromUnit.equals("oz") && toUnit.equals("g")) {
+				System.out.print(measurement*28.3495);
+			} else if (fromUnit.equals("kg") && toUnit.equals("oz")) {
+				System.out.print(measurement*35.274);
+			} else if (fromUnit.equals("oz") && toUnit.equals("kg")) {
+				System.out.print(measurement/35.274);
+			} return measurement;
 	}
 	public static void main(String[] args)
 	{
@@ -93,13 +97,16 @@ public class Unit
 
 		System.out.print("Measurement: ");
 		double measurement = keyboard.nextDouble();
-		System.out.print("Take the numbers before the first period ");
+		//System.out.print("Take the numbers before the first period ");
 		
 		
-		
+		if(fromUnit.equals("m") || fromUnit.equals("km") ||(fromUnit.equals("mi")))
 		measurement = lengthConversion(measurement, fromUnit, toUnit);
+		else if(fromUnit.equals("g") || fromUnit.equals("kg") || (fromUnit.equals("g") || fromUnit.equals("kg")))
 		measurement = massConversion(measurement, fromUnit, toUnit);
+		else if(fromUnit.equals("F") || fromUnit.equals("C") || fromUnit.equals("K"))
 		measurement = temperatureConversion(measurement, fromUnit, toUnit);
+		else if(fromUnit.equals("sec") || fromUnit.equals("min") || fromUnit.equals("hr"))
 		measurement = timeConversion(measurement, fromUnit, toUnit);
 
 	}
